566897e889d5e8095b2ee83cf1a01ac2df1e29d3
xingzerun <xingzerun@users.noreply.github.com>
2017-07-04 11:09:18 +0800

    Create countCity.py

---
+++ countCS/countCity.py
@@ -0,0 +1,44 @@
+from requests import post
+from pymongo import MongoClient
+
+client = MongoClient(API) #需要数据库地址
+db = client['chatLog']
+users = db['usercontacts']
+
+for user in users.find():
+    userPhone = user['userPhone']
+    userContacts = user['userContacts']
+    contactSex = []
+    try:
+        for i in range(len(userContacts)):
+            if 'contactSex' in userContacts[i].keys():
+                x = userContacts[i]['contactSex']
+                if x == 0:
+                    x = '未知'
+                elif x == 1:
+                    x = '男'
+                elif x == 2:
+                    x = '女'
+                else:
+                    pass
+                contactSex.append(x)
+            else:
+                pass
+    except:
+        pass
+
+    countList = []
+    countStr = ''
+    countSex = set(contactSex)
+    for i in countSex:
+        countList.append("%s=%d" % (i, contactSex.count(i)))
+    countStr = '|'.join(countList)
+
+    d = {
+        'mobile': userPhone,
+        'list': countStr
+    }
+
+    url = API_mobile #提交城市数量统计的接口
+    r = post(url, data=d)
+    print(userPhone,':',r.content)
